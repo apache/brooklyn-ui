5a202896d41a0a9cf27bc1905a7d9012fd07f9a2
Yvan Bourrut <yvan.bourrut@usharesoft.com>
2019-06-12 16:39:55 +0200

    UFORGE-13892 As a user, I want to keep the state of the line folded in the blueprint yaml editor

--- ui-modules/utils/yaml-editor/yaml-editor.js
+++ ui-modules/utils/yaml-editor/yaml-editor.js
@@ -52,6 +52,7 @@ export function yamlEditorDirective($rootScope, brSnackbar) {
 
     function link($scope, $element) {
         $scope.type = $scope.type || 'brooklyn';
+        $scope.CODE_FOLDER_STORAGE_NAME = $scope.CODE_FOLDER_STORAGE_NAME || 'blueprint-composer.code-folder';
 
         // Init CodeMirror
         CodeMirror.extendMode('yaml', {
@@ -149,6 +150,120 @@ export function yamlEditorDirective($rootScope, brSnackbar) {
                 widget: '{ ... }'
             }
         });
+        window.cm=$scope.cm //TODO remove me
+
+        function getDataFromLocalStorage(){
+            if ($scope.CODE_FOLDER_STORAGE_NAME) {
+                return JSON.parse(localStorage.getItem($scope.CODE_FOLDER_STORAGE_NAME))
+            } else {
+                return null;
+            }
+        }
+
+        function setDataToLocalStorage(data){
+            if ($scope.CODE_FOLDER_STORAGE_NAME) {
+                localStorage.setItem($scope.CODE_FOLDER_STORAGE_NAME, JSON.stringify(data));
+            }
+        }
+
+        function extractKey(line) {
+            line = line.split(':')[0].trim()
+            if (line[0] == '-') {
+                line = line.substr(1).trim()
+            }
+            return line
+        }
+        
+        function findLine(startLine, indentLvlMinimal, foldBlock, indexFoldBlock) {
+            let entryInArray = -1;
+            let i = startLine;
+            while (i < $scope.cm.lineCount() && $scope.cm.lineInfo(i).handle.stateAfter.keyCol > indentLvlMinimal) {
+                if (Number.isInteger(foldBlock[indexFoldBlock])) {
+                    if ($scope.cm.lineInfo(i).text.trim()[0] == '-') {
+                        entryInArray++;
+                    }
+                    if (entryInArray == foldBlock[indexFoldBlock]) {
+                        if (indexFoldBlock+1 == foldBlock.length) {
+                            return i;
+                        } else {
+                            return findLine(i, $scope.cm.lineInfo(i).handle.stateAfter.keyCol-1, foldBlock, indexFoldBlock+1)
+                        }
+                    }
+                } else {
+                    if (extractKey($scope.cm.lineInfo(i).text) == extractKey(foldBlock[indexFoldBlock])) {
+                        if (indexFoldBlock+1 == foldBlock.length) {
+                            return i;
+                        } else {
+                            return findLine(i+1, $scope.cm.lineInfo(i).handle.stateAfter.keyCol, foldBlock, indexFoldBlock+1)
+                        }
+                    }
+                }
+                i++;
+            }
+            return -1
+        }
+
+        function fold(foldBlock) {
+            let line = findLine(0, -1, foldBlock, 0)
+            if (line != -1) {
+                $scope.cm.foldCode(line, {}, 'fold')
+            }
+        }
+
+        function foldAll(foldArray) {
+            console.log(foldArray)
+            if (foldArray) {
+                foldArray.forEach((value) => fold(value))
+            }
+        }
+        foldAll(getDataFromLocalStorage())
+
+        function findParents(line, lineNumber) {
+            let currentIndent = line.stateAfter.keyCol
+            if (currentIndent > 0) {
+                let i = lineNumber
+                let indexArray = -1
+                while (currentIndent <= line.parent.lines[i].stateAfter.keyCol) {
+                    if (currentIndent == line.parent.lines[i].stateAfter.keyCol && line.parent.lines[i].text.trim()[0] == '-') {
+                        indexArray++;   
+                    }
+                    i--
+                }
+                let current
+                if (line.text.trim()[0] == '-') {
+                    current = indexArray
+                } else {
+                    current = extractKey(line.text)
+                }
+                let parents = findParents(line.parent.lines[i], i)
+                parents.push(current)
+                return parents
+            } else {
+                return [extractKey(line.text)]
+            }
+        }
+
+        function findFold() {
+            let value = []
+            let i = 0
+            while (i < $scope.cm.lineCount()) {
+                let line = $scope.cm.lineInfo(i)
+                if ($scope.cm.isFolded(line) && line.gutterMarkers && line.gutterMarkers['CodeMirror-foldgutter'] 
+                    && line.gutterMarkers['CodeMirror-foldgutter'].classList.contains('CodeMirror-foldgutter-folded')) {
+                    value.push(findParents(line.handle.parent.lines[line.line], line.line))
+                }
+                i++;
+            }
+            return value;
+        }
+
+        function onFoldChanged() {
+            setDataToLocalStorage(findFold())
+        }
+        $scope.cm.on('fold', onFoldChanged);
+        $scope.cm.on('unfold', onFoldChanged);
+
+
         $scope.cm.on('changes', (cm, change)=> {
             if ($scope.value !== cm.getValue()) {
                 $scope.$apply(()=> {
