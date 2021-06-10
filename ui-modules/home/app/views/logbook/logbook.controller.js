f2dd70d4aa128b9ee19bedf677af3114b79848ca
Juan Cabrerizo <juan@cloudsoft.io>
2021-06-10 16:42:19 +0100

    Updated up to show datepickers

--- ui-modules/home/app/views/logbook/logbook.controller.js
+++ ui-modules/home/app/views/logbook/logbook.controller.js
@@ -59,54 +59,115 @@ export function logbookStateController($scope, brBrandInfo, version, states, log
         vm.doQuery();
     });
 
+    vm.getChecked = function (group) {
+        let levels = [];
+        group.forEach(function (item) {
+            if (item.selected) levels.push(item.value);
+        });
+        return levels;
+    }
     vm.doQuery = function () {
         $scope.waitingResponse = true;
+        $scope.results = "Loading..."
 
-        let from = $scope.from;
-        if($scope.reverseOrder){
-            from = from === 0? -1 : -from
-        }
+        const levels = $scope.allLevels ? ['ALL']: vm.getChecked($scope.logLevels);
+
         const params = {
-            from:from,
-            numberOfItems:$scope.numberOfItems,
-            logLevel:$scope.queryLevel
+            reverseOrder: $scope.reverseOrder,
+            numberOfItems: $scope.numberOfItems,
+            levels: levels,
+            initTime: $scope.initTime,
+            finalTime: $scope.finalTime,
         }
+
         logbookApi.logbookQuery(params, true).then(function (success) {
             // TODO implement logic for make output as table
             $scope.results = vm.createLogOutputAsText(success);
         }, function (error) {
-            $scope.results = "Error getting the logs: \n" + JSON.stringify(error);
+            $scope.results = "Error getting the logs: \n" + error.error.message;
+            console.log(JSON.stringify(error));
         }).finally(() => {
             $scope.waitingResponse = false;
         });
     };
 
     vm.createLogOutputAsText = function (success) {
-        let output=[];
-        success.forEach(entry=>{
+        let output = [];
+        const fieldsToShow = vm.getChecked($scope.logFields);
+        success.forEach(entry => {
             let outputLine = [];
-            outputLine.push(entry.timestamp || "");
-            outputLine.push(entry.taskId || "");
-            outputLine.push(entry.entityIds || "");
-            outputLine.push(entry.level || "");
-            outputLine.push(entry.bundleId || "");
-            outputLine.push(entry.class || "");
-            outputLine.push(entry.threadName || "");
-            outputLine.push(entry.message || "");;
+            if (fieldsToShow.includes("datetime"))
+                outputLine.push(entry.timestamp || "");
+            if (fieldsToShow.includes("taskId"))
+                outputLine.push(entry.taskId || "");
+            if (fieldsToShow.includes("entityIds"))
+                outputLine.push(entry.entityIds || "");
+            if (fieldsToShow.includes("level"))
+                outputLine.push(entry.level || "");
+            if (fieldsToShow.includes("bundleId"))
+                outputLine.push(entry.bundleId || "");
+            if (fieldsToShow.includes("class"))
+                outputLine.push(entry.class || "");
+            if (fieldsToShow.includes("threadName"))
+                outputLine.push(entry.threadName || "");
+            if (fieldsToShow.includes("message"))
+                outputLine.push(entry.message || "");
+
             output.push(outputLine.join(" "));
         })
         return output.join("\n");
     }
 
-    $scope.results = "-empty-";
-    $scope.from = 0;
-    $scope.numberOfItems = 10;
-    $scope.waitingResponse = false;
-    $scope.logLevels = [{"name": "All", "value": "ALL"}, {"name": "Debug", "value": "DEBUG"}, {
-        "name": "Info",
-        "value": "INFO"
+    vm.resetForm = function () {
+        $scope.numberOfItems = 10;
+        $scope.allLevels = true
+        $scope.logLevels = [
+            {"name": "Debug", "value": "DEBUG", "selected": false},
+            {"name": "Info", "value": "INFO ", "selected": false},
+            {"name": "Warn", "value": "WARN ", "selected": false},
+            {"name": "Error", "value": "ERROR", "selected": false},
+            {"name": "Fatal", "value": "FATAL", "selected": false},
+        ];
+        $scope.fieldsToShow = ['datetime', 'class', 'message']
+        $scope.logFields = [
+            {"name": "Timestamp", "value": "datetime", "selected": true},
+            {"name": "Task ID", "value": "taskId", "selected": false},
+            {"name": "Entity ID", "value": "entityIds", "selected": false},
+            {"name": "Log level", "value": "level", "selected": true},
+            {"name": "Bundle ID", "value": "bundleId", "selected": false},
+            {"name": "Class", "value": "class", "selected": true},
+            {"name": "Thread name", "value": "threadName", "selected": false},
+            {"name": "Message", "value": "message", "selected": true},
+        ];
+        $scope.reverseOrder = false;
+        $scope.initTime = "";
+        $scope.finalTime = "";
+    }
 
-    }, {"name": "Warn", "value": "WARN"}, {"name": "Error", "value": "ERROR"}, {"name": "Fatal", "value": "FATAL"}];
-    $scope.queryLevel = $scope.logLevels[0].value;
-    $scope.reverseOrder = true;
+    $scope.$watch('allLevels', function (v) {
+        if (!v){
+            if(vm.getChecked($scope.logLevels).length===0){
+                $scope.allLevels = true;
+            }else{
+                return;
+            }
+        }
+        for (let i = 0; i < $scope.logLevels.length; ++i) {
+            $scope.logLevels[i].selected = false;
+        }
+    });
+    $scope.$watch('logLevels', function (newVal, oldVal) {
+        var selected = newVal.reduce(function (s, c) {
+            return s + (c.selected ? 1 : 0);
+        }, 0);
+        if (selected === newVal.length || selected ===0) {
+            $scope.allLevels = true;
+        } else if (selected > 0) {
+            $scope.allLevels = false;
+        }
+    }, true);
+
+    $scope.waitingResponse = false;
+    vm.resetForm();
+    $scope.results = "-empty-"
 }
