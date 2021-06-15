a5a4a99a32d16b396e3f38ba4481e4114701fa81
iuliana <iuliana@cloudsoft.io>
2021-06-15 11:29:34 +0100

    Smart-121 part 2 fix. Graphical editor needs original yaml, no format.

--- ui-modules/blueprint-composer/app/views/main/main.controller.js
+++ ui-modules/blueprint-composer/app/views/main/main.controller.js
@@ -153,9 +153,13 @@ export function MainController($scope, $element, $log, $state, $stateParams, brB
             // or in other words, for other items, DO set these
             vm.saveToCatalogConfig = Object.assign(vm.saveToCatalogConfig, vm.saveToCatalogConfig.original); 
         }
-        
-        yaml = edit.type.plan.data;
+
         $scope.initialYamlFormat = edit.type.plan.format;
+        if($scope.initialYamlFormat && Array.isArray(edit.type.specList) && edit.type.specList.length > 0 && edit.type.specList[0].format === $scope.initialYamlFormat) {
+            yaml = edit.type.specList[0].contents;
+        }  else {
+            yaml = edit.type.plan.data;
+        }
     }
 
     vm.isTabActive = stateKey => {
