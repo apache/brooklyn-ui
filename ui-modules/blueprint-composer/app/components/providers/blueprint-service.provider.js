511a59d1d0252819c48a99f3f33832b69d20066a
Mykola Mandra <mykola.mandra@cloudsoft.io>
2021-07-19 12:53:41 +0100

    Do not concatenate relationships twice in relationship provider
    Signed-off-by: Mykola Mandra <mykola.mandra@cloudsoft.io>

--- ui-modules/blueprint-composer/app/components/providers/blueprint-service.provider.js
+++ ui-modules/blueprint-composer/app/components/providers/blueprint-service.provider.js
@@ -110,11 +110,11 @@ function BlueprintService($log, $q, $sce, paletteApi, iconGenerator, dslService,
 
     // Add relationships provider based on Entity spec
     addEntityRelationshipsProvider('spec', {
-        apply: (entity, relationships) => {
+        apply: (entity) => {
             return Array.from(entity.config.values())
                 .filter(config => config && config[DSL_ENTITY_SPEC] && config[DSL_ENTITY_SPEC] instanceof Entity)
                 .map(config => config[DSL_ENTITY_SPEC])
-                .reduce((relationships, spec) => relationships.concat(getRelationships(spec)), relationships);
+                .reduce((relationships, spec) => relationships.concat(getRelationships(spec)), []);
         }
     });
 
@@ -832,7 +832,7 @@ function BlueprintService($log, $q, $sce, paletteApi, iconGenerator, dslService,
 
         // Aggregate relationships discovered by Entity relationships providers.
         for (let provider of Object.values(entityRelationshipProviders)) {
-            relationships = relationships.concat(provider.apply(entity, relationships));
+            relationships = relationships.concat(provider.apply(entity));
         }
 
         // Iterate over children and reduct.
